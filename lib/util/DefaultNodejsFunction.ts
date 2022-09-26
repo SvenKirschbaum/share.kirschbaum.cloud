d75d1556e6c992ccbd387724049e38e08ee63183
Sven Kirschbaum <sven@kirschbaum.me>
2022-09-26 20:23:49 +0200

    :fire: Remove lambda layer, as selective bundling will likely be more efficient

--- lib/util/DefaultNodejsFunction.ts
+++ lib/util/DefaultNodejsFunction.ts
@@ -1,38 +1,16 @@
 import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import {
-  Architecture, Code, LayerVersion, Runtime, Tracing,
+  Architecture, Runtime, Tracing,
 } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { Stack } from 'aws-cdk-lib';
-// eslint-disable-next-line import/no-relative-packages
-import * as nodePackageJson from '../../lambda/nodejs/package.json';
 
 export default class DefaultNodejsFunction extends NodejsFunction {
   constructor(scope: Construct, id: string, props: NodejsFunctionProps) {
-    const layerId = 'default-node-layer';
-
-    const stack = Stack.of(scope);
-    let layer = stack.node.tryFindChild(layerId) as LayerVersion;
-
-    layer = layer || new LayerVersion(stack, layerId, {
-      code: Code.fromAsset('lambda', {
-        exclude: ['nodejs/src', 'nodejs-edge'],
-      }),
-      compatibleRuntimes: [Runtime.NODEJS_14_X, Runtime.NODEJS_16_X],
-    });
-
     super(scope, id, {
       ...props,
-      bundling: {
-        externalModules: [
-          'aws-sdk',
-          ...Object.keys(nodePackageJson.dependencies),
-        ],
-      },
       runtime: Runtime.NODEJS_16_X,
       architecture: Architecture.ARM_64,
-      layers: [layer],
       logRetention: RetentionDays.TWO_WEEKS,
       tracing: Tracing.ACTIVE,
     });
