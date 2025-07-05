b273b8af921ec22bb1fe76e1012e5f6c240c2b6d
Sven Kirschbaum <sven@kirschbaum.me>
2025-07-06 00:45:11 +0200

    fix: Remove splitVendorChunkPlugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import {defineConfig, splitVendorChunkPlugin} from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgrPlugin(), basicSsl(), splitVendorChunkPlugin()],
+    plugins: [react(), svgrPlugin(), basicSsl()],
     server: {
         port: 3000,
         // Required to work with oauth provider
